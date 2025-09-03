6920420d088f66f847595a2594ba3d70c7bebe6e
mbarraldevs <mbarraldevs@outlook.com>
2025-09-03 12:18:43 +0200

    Transaction calldata to memmory

--- src/zksync/ZkMinimalAccount.sol
+++ src/zksync/ZkMinimalAccount.sol
@@ -6,25 +6,25 @@ import {Transaction} from
     "lib/foundry-era-contracts/src/system-contracts/contracts/libraries/MemoryTransactionHelper.sol";
 
 contract ZkMinimalAccount is IAccount {
-    function validateTransaction(bytes32 _txHash, bytes32 _suggestedSignedHash, Transaction calldata _transaction)
+    function validateTransaction(bytes32 _txHash, bytes32 _suggestedSignedHash, Transaction memory _transaction)
         external
         payable
         returns (bytes4 magic)
     {}
 
-    function executeTransaction(bytes32 _txHash, bytes32 _suggestedSignedHash, Transaction calldata _transaction)
+    function executeTransaction(bytes32 _txHash, bytes32 _suggestedSignedHash, Transaction memory _transaction)
         external
         payable
     {}
 
-    function executeTransactionFromOutside(Transaction calldata _transaction) external payable {}
+    function executeTransactionFromOutside(Transaction memory _transaction) external payable {}
 
-    function payForTransaction(bytes32 _txHash, bytes32 _suggestedSignedHash, Transaction calldata _transaction)
+    function payForTransaction(bytes32 _txHash, bytes32 _suggestedSignedHash, Transaction memory _transaction)
         external
         payable
     {}
 
-    function prepareForPaymaster(bytes32 _txHash, bytes32 _possibleSignedHash, Transaction calldata _transaction)
+    function prepareForPaymaster(bytes32 _txHash, bytes32 _possibleSignedHash, Transaction memory _transaction)
         external
         payable
     {}
